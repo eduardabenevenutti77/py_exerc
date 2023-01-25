7b9b93173285ac21ad3c749c85feec761614a7f2
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-01-25 08:41:30 -0300

    Add files via upload

--- desafio001.py
+++ desafio001.py
@@ -1,8 +1,8 @@
 #mostre o antecessor e sucessor de um número
-
-
-
-
+num = int(input('digite um número'))
+ant = num - 1
+suc = num + 1
+print('o antecessor é {} e o sucessor é {} do número {}'.format(ant, suc, num))
 
 #mostre o dobro, o triplo e a raiz quadrada do número
 n = int(input('informe um valor para a operação:'))
@@ -11,18 +11,11 @@ t = n * 3
 r = n ** 0.25
 print('o dobro de {} é {}, o triplo {} e a sua raiz é {}'.format(n, d, t, r))
 
-
-
-
-
 #peça 2 notas e calcule e mostre a media do aluno
 nota1 = float(input('digite a primeira nota:'))
 nota2 = float(input('digite a segunda nota:'))
 m = (nota1 + nota2) / 2
 print('a média do aluno é {}'. format(m))
-
-
-
 
 #calcule a area de uma parede e mostre a quantidade de tinta utilizada para a sua pintura, mas considerando que cada l
 #pinta 2m
@@ -34,13 +27,12 @@ print('a area é {}'.format(area))
 l = (area * 1000)
 print('a quantidade de tinta a ser gasta é {}'.format(l))
 
-
-
-
-
 #peça o preço e de 5% de desconto
-
-
-
+preco = int(input('qual é o valor da peça?'))
+des = 5 / 100 * preco
+print('o desconto será de 5%, então o desconto sobre peça irá custar {} reais'.format(des))
 
 #peça o salário e de 15% de aumento
+sa = float(input('qual é o seu salário?'))
+aumento = 0.15 * sa
+print('o seu salário terá um aumento de {}'.format(aumento))
--- desafio002.py
+++ desafio002.py
@@ -1,31 +1,31 @@
 #peça um valor em metros e converta em cm e ml
-
-
-
-
-
-#peça um valor e faça a sua tabuada
-
-
-
-
-#peça quantos de dinheiro (real) a pessoa tem e converta em dólares 1 = 3,27
-
-
-
-
-#peça um valor em metros e converta em cm e ml
-
-
-
-
+metros = int(input('informe o valor que será transformado em centímetros e milimetros:'))
+cm = (metros * 100)
+ml = (metros * 1000)
+print('{} metros transformado em centrímetros é {} e em milimetros é {}'.format(metros, cm, ml))
 
 #peça um valor e faça a sua tabuada
+tabu = int(input('escolha qual tabuada será realizada:'))
+tab = (tabu * 0)
+tab1 = (tabu * 1)
+tab2 = (tabu * 2)
+tab3 = (tabu * 3)
+tab4 = (tabu * 4)
+tab5 = (tabu * 5)
+tab6 = (tabu * 6)
+tab7 = (tabu * 7)
+tab8 = (tabu * 8)
+tab9 = (tabu * 9)
+tab0 = (tabu * 10)
 
-
-
+print('a tabuada do {} é {}, {}, {}, {}, {}.{}, {}, {}, {}, {}'.format(tabu, tab, tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, tab9, tab0))
 
 #peça quantos de dinheiro (real) a pessoa tem e converta em dólares 1 = 3,27
+real = float(input('qual valor em reais voce deseja converter para dólares?'))
+cota = (real / 5)
+print('voce terá em dóleres:{}'.format(cota))
+
+
 
 
 
